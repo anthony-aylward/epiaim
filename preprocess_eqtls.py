8af31fe1e862c2ccd7f545100a6bb2573eacd489
anthony-aylward <aaylward@eng.ucsd.edu>
2020-04-13 22:39:19 -0700

    update

---
+++ preprocess_eqtls.py
@@ -0,0 +1,25 @@
+import funcgenom
+import os
+import sys
+
+sys.path.append('/home/data/kglab-python3-modules')
+import colocalize
+
+from argparse import ArgumentParser
+
+def parse_arguments():
+    parser = ArgumentParser(description='preprocess islet eQTL data')
+    parser.add_argument(
+        'gene_ids',
+        metavar='gene_ids.txt',
+        help='gene ids'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    genome = funcgenom.Genome()
+    with open(args.gene_ids, 'r') as f:
+        gene_ids = f.splitlines()
+    colocalize.load_islet_eqtl_data(genome, gene_ids, processes=23)
